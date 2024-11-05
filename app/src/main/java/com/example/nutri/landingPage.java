e9c5700752790f1072e2526bfaed462485209aeb
MattSouza14 <mateussouzabr1@gmail.com>
2024-11-05 19:41:34 -0300

    Alterações - Iarley

---
+++ app/src/main/java/com/example/nutri/landingPage.java
@@ -0,0 +1,22 @@
+package com.example.nutri;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class landingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_landing_page);
+
+    }
+}
